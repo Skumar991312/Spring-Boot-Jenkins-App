ff2c4526cff68443f8341410e24be6ced8014025
SANDEEP KUMAR <sdeepmail419@gmail.com>
2025-06-07 01:23:11 +0530

    config

--- src/main/java/com/jenkins/rest/UserRestController.java
+++ src/main/java/com/jenkins/rest/UserRestController.java
@@ -1,15 +1,16 @@
 package com.jenkins.rest;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserRestController {
 	
-	@GetMapping("/wel")
-	public String getMsg() {
-		String name="Welcome To SpringBoot Applications";
-		return name;
+	@GetMapping("/user")
+	public String getMsg() { 
+	return "Welcome To SpringBoot Applications";
+	
 	}
 
 }
